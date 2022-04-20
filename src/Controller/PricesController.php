62ef8fbe114f5224f58bb7bd5830d228655df927
antoinelbc <antoine.lebourdonnec8@gmail.com>
2022-04-20 15:22:15 +0200

    navbar created

---
+++ src/Controller/PricesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PricesController extends AbstractController
+{
+    #[Route('/prices', name: 'app_prices')]
+    public function index(): Response
+    {
+        return $this->render('prices/index.html.twig', [
+            'controller_name' => 'PricesController',
+        ]);
+    }
+}
