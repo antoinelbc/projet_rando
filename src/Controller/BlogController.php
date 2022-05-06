9a9133d9d6b8091990b310500ec1fa51f4ca2506
antoinelbc <antoine.lebourdonnec8@gmail.com>
2022-05-06 15:28:25 +0200

    home template add blogcontroller and article admin part

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+//
+use App\Repository\ArticlesRepository;
+
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(ArticlesRepository $articlesRepository): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articlesRepository->findAll(),
+        ]);
+    }
+}
